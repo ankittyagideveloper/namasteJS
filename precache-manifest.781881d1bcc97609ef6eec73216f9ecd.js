8d7a864b779d6e0990a03219517f971e79dc1553
Ankit Tyagi <you@example.com>
2022-05-29 02:30:13 +0530

    Updates

---
+++ precache-manifest.781881d1bcc97609ef6eec73216f9ecd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f84cdcb5a05c444d4bea2fbedc00b96c",
+    "url": "/namasteJS/index.html"
+  },
+  {
+    "revision": "cc5324a3e5d8e6cba4b3",
+    "url": "/namasteJS/static/css/main.c53019a0.chunk.css"
+  },
+  {
+    "revision": "8e753d47a70fe2de14bd",
+    "url": "/namasteJS/static/js/2.093fd4c4.chunk.js"
+  },
+  {
+    "revision": "cc5324a3e5d8e6cba4b3",
+    "url": "/namasteJS/static/js/main.8926c9b0.chunk.js"
+  },
+  {
+    "revision": "0e2b9210bbb4a974ac3e",
+    "url": "/namasteJS/static/js/runtime~main.4f08cf33.js"
+  },
+  {
+    "revision": "5e93cacb5637307f911c0dc3cde21983",
+    "url": "/namasteJS/static/media/Logo.5e93cacb.jpg"
+  }
+]);
